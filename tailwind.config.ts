684433d3d13ce912a9c37c63c510573b749a880d
Thomas <nyumat18@gmail.com>
2024-01-30 21:24:51 -0800

    ✨ styling: tremor/react and dark mode support

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,45 @@
 import type { Config } from "tailwindcss";
-
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/(components)/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: ["class"],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "gray-1": "var(--gray-1)",
+        "gray-2": "var(--gray-2)",
+        "gray-3": "var(--gray-3)",
+        background: "var(--background-color)",
+        foreground: "var(--foreground)",
+
+        tremor: {
+          background: {
+            DEFAULT: "var(--background-color)",
+          },
+          border: {
+            DEFAULT: "var(--gray-2)",
+          },
+          ring: {
+            DEFAULT: "var(--gray-2)",
+          },
+          content: {
+            subtle: "var(--gray-2)",
+            DEFAULT: "var(--gray-1)",
+            emphasis: "var(--foreground)",
+          },
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("@headlessui/tailwindcss"),
+    require("@tailwindcss/forms"),
+    require("tailwindcss-animate"),
+  ],
 };
 export default config;
